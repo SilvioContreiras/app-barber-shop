fc8ba120c0f3e324eb2538c7f160d20b2955066d
Silvio <silvio.contreiras@trademastersp.com.br>
2020-07-05 20:10:28 -0300

    chore(server): change server to use express

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,4 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
+routes.post('/users', (req, res) => {
+  const { name, age } = req.body;
+
+  const user = {
+    name,
+    age,
+  };
+
+  return res.json(user);
+});
+
 export default routes;
