bd85e2265df62edac60ae9266c6db076280ad307
Silvio <silvio.contreiras@trademastersp.com.br>
2020-07-05 18:56:43 -0300

    feat: initial config

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Hello TS' }));
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
